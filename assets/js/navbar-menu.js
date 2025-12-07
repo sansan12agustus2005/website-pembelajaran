12729c641cc9dec5790469d32ed9f12a28cfef46
Sandi Nugraha <sandiinugraha128.com>
2025-12-07 10:28:31 +0700

    Update: terbaru

--- assets/js/navbar-menu.js
+++ assets/js/navbar-menu.js
@@ -1,59 +1,3 @@
-<<<<<<< HEAD
-const navmenu = [
-  {
-    title: "Beranda",
-    href: "index.html",
-  },
-  {
-    title: "Profil Sekolah",
-    href: "profilsekolah.html",
-  },
-  {
-    title: "Tujuan Web",
-    href: "tujuan.html",
-  },
-  {
-    title: "Profil Pembuat",
-    href: "profilpembuat.html",
-  },
-  {
-    title: "Absensi & Evaluasi",
-    href: "kelas-evaluasi.html",
-  },
-  {
-    title: "Materi & Quis",
-    href: "materi_quies.html",
-  },
-  {
-    title: "Sosial Media",
-    href: "sosial-media.html",
-  }
-];
-
-const navMenuComponent = document.getElementById("navbar-component")
-
-navMenuComponent.innerHTML =`  <div class="navbar">
-    <div class="navbar-left">
-      <img src="/web-pembelajaran/assets/images/LOGO.png" class="logo-img" alt="Logo Sekolah">
-      <span class="logo-text">WEB PEMBELAJARAN</span>
-    </div>
-
-
-    <ul class="navbar-menu">
-    ${navmenu.map(menu => `
-  <li><a href="${menu.href}">${menu.title}
-  </a>
-  </li>
-`).join("")}
-    
-    </ul>
-  </div>`
-
-
-
-
-
-=======
 const navmenu = [
   {
     title: "Beranda",
@@ -108,4 +52,3 @@ navMenuComponent.innerHTML =`  <div class="navbar">
 
 
 
->>>>>>> 1973c2c8288b0861689e758ee58580016215ac12
